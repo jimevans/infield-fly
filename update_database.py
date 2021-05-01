3e31973ad6f926b91a78d207b3755a033f5fdd75
Jim Evans <james.h.evans.jr@gmail.com>
2021-05-01 12:55:13 -0400

    feat: Allow force updates of all tracked series at command line

--- update_database.py
+++ update_database.py
@@ -1,10 +1,16 @@
 """Updates the metadata database"""
 
+import argparse
 from config_settings import Configuration
 from episode_database import EpisodeDatabase
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--force-updates", action="store_true", default=False,
+                    help="Forces updates of metadata of all tracked series, including ended ones")
+
+args = parser.parse_args()
 
 config = Configuration()
 episode_db = EpisodeDatabase.load_from_cache(config.metadata)
-episode_db.update_all_tracked_series()
+episode_db.update_all_tracked_series(force_updates=args.force_updates)
 episode_db.save_to_cache()
