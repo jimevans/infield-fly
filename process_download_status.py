c72f11acc6f858866cacce77e966298292b358d6
Jim Evans <james.h.evans.jr@gmail.com>
2021-09-07 21:46:52 +0000

    fix: Update argument name

--- process_download_status.py
+++ process_download_status.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 import argparse
-from sys import argv
 from job_queue import JobQueue
 
 parser = argparse.ArgumentParser()
 parser.add_argument("torrent_hash", help="Torrent hash")
 parser.add_argument("torrent_name", help="Torrent name")
-parser.add_argument("torrent_dirctory", help="Torrent directory")
+parser.add_argument("torrent_directory", help="Torrent directory")
 
 args = parser.parse_args()
 
 queue = JobQueue.load_from_cache()
 queue.update_download_job(args.torrent_hash, args.torrent_name, args.torrent_directory)
+
