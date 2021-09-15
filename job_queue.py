54377e6e07e1a531d2a3e68213e37fec9303b2a2
Jim Evans <james.h.evans.jr@gmail.com>
2021-09-15 10:53:42 -0400

    fix: Update job queue for job creation

--- job_queue.py
+++ job_queue.py
@@ -51,8 +51,9 @@ class JobQueue:
         job_to_add.keyword = keyword
         job_to_add.query = query
         job_to_add.added = datetime.now().strftime("%Y-%m-%d")
-        job_to_add.status = "searching"
+        job_to_add.status = "waiting"
         self.add_job(job_to_add)
+        self.save_to_cache()
 
     def perform_conversions(self):
         config = Configuration()
@@ -111,7 +112,12 @@ class JobQueue:
                                 "s{:02d}e{:02d}".format(
                                     series_episode.season_number, series_episode.episode_number))
                         if not self.is_existing_job(tracked_series.main_keyword, search_string):
-                            self.create_job(tracked_series.main_keyword, search_string)
+                            job_to_add = Job({})
+                            job_to_add.keyword = keyword
+                            job_to_add.query = query
+                            job_to_add.added = datetime.now().strftime("%Y-%m-%d")
+                            job_to_add.status = "searching"
+                            self.add_job(job_to_add)
 
             staging_directory = config.conversion.staging_directory
             finder = TorrentDataProvider()
